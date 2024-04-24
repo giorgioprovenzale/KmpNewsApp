c12ff4742ff6a990d6e0a4ff6e593e2b2ad92d67
Giorgio Provenzale <giorgio.provenzale@gmail.com>
2024-04-24 17:18:42 +0200

    uncomment dependency

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -57,7 +57,7 @@ kotlin {
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material)
-            //implementation(compose.material3)
+            implementation(compose.material3)
             implementation(compose.ui)
             implementation(compose.components.resources)
 
